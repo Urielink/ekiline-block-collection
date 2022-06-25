3637ca05a8f0ce9f76349012d1bf1edb1a6dd456
Ricardo Pineda Lazcano <urielink81@gmail.com>
2022-06-25 16:20:44 -0500

    ajuste no se requiere css al frente.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'da854a9566b5677d64b5');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '7d3b37caf9688a47c7e2');
