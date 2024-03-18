f3a67758f8b5dd900b40ba676fe7d09eac447637
Urielink <urielink81@gmail.com>
2024-03-17 18:53:09 -0600

    node 20

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-<?php
-
-return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-rich-text', 'wp-server-side-render'), 'version' => '8c5a3a109da1a3929605');
-=======
 <?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-rich-text', 'wp-server-side-render'), 'version' => '33c7c1b89f07a69bd69c');
->>>>>>> a4fedba (actualizacion node)
