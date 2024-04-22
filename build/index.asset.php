d36124867b5b92c951de4789e0f35212f79f32aa
Urielink <urielink81@gmail.com>
2024-04-22 00:16:46 -0600

    veredicto rehacer.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 <?php
 
 return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-rich-text', 'wp-server-side-render'), 'version' => '33c7c1b89f07a69bd69c');
+=======
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-rich-text', 'wp-server-side-render'), 'version' => '8b62183af1fc2d73a302');
+>>>>>>> ce596e9 (veredicto rehacer.)
