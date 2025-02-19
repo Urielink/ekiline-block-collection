46ce27851425f7d3ffd67839ab572feb6ed5d30d
Urielink <urielink81@gmail.com>
2025-02-18 22:22:13 -0600

    test

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,3 +1,1 @@
-<?php
-
-return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-rich-text', 'wp-server-side-render'), 'version' => '307fa80ece14404b2cd8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-rich-text', 'wp-server-side-render'), 'version' => '307fa80ece14404b2cd8');
