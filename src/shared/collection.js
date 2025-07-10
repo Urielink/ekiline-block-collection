0d61c06eb667b5279fd6497fbf51509e85d50fb3
Urielink <urielink81@gmail.com>
2025-07-09 21:51:28 -0600

    apartado de funciones compartidas

---
+++ src/shared/collection.js
@@ -0,0 +1,30 @@
+/**
+ * Asignar colección.
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
+ */
+import { registerBlockCollection } from '@wordpress/blocks';
+
+export const registerEkilineCollection = (icon) => {
+	registerBlockCollection('ekiline-block-collection', {
+		title: 'Ekiline Block Collection',
+		icon: icon,
+	});
+};
+
+/**
+ * 
+ * @param {*} min 
+ * @param {*} max 
+ * @returns número aleatorio entre min y max.
+ * 
+ * import { getRandomArbitrary } from '../../shared/utils';
+ * 
+ * if (!attributes.anchor) {
+ *  setAttributes({ anchor: 'accordion' + getRandomArbitrary(10, 150) });
+ * }
+ * 
+ */
+export function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
