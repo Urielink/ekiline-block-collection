e8b247a5e9a51a09382252cf5a57265c39a6e735
Urielink <urielink81@gmail.com>
2025-07-24 21:53:51 -0600

    controles agregados para personalizar carrusel.

--- build/components/carousel/ekiline-carousel/index.asset.php
+++ build/components/carousel/ekiline-carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd5708e2441b978395d1a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7faa9bf6e5f50a2f2373');
--- src/components/carousel/ekiline-carousel/edit.js
+++ src/components/carousel/ekiline-carousel/edit.js
@@ -1,6 +1,6 @@
 import { __ } from '@wordpress/i18n'
 import { InspectorControls } from '@wordpress/block-editor'
-import { PanelBody, SelectControl, ToggleControl } from '@wordpress/components'
+import { PanelBody, SelectControl, ToggleControl, TextControl, RangeControl } from '@wordpress/components'
 // manual
 import { ManualEdit } from './variations/manual'
 // galeria
@@ -37,7 +37,7 @@ export default function Edit ({ attributes, setAttributes }) {
     <>
       <InspectorControls>
         {/* Seleccionar tipo de carrusel: */}
-        <PanelBody title={__('Carousel Type', 'ekiline-block-collection')} initialOpen={true}>
+        <PanelBody title={__('Carousel Type', 'ekiline-block-collection')} initialOpen={false}>
           <SelectControl
             label={__('Choose the carousel mode', 'ekiline-block-collection')}
             value={ChooseType}
@@ -114,8 +114,74 @@ export default function Edit ({ attributes, setAttributes }) {
               checked={attributes.contentIsDynamic}
               onChange={(value) => setAttributes({ contentIsDynamic: value })}
             />
+
           </PanelBody>
         )}
+
+        {/* Atributos de look */}
+        <PanelBody title={__('Carousel Look', 'ekiline-block-collection')} initialOpen={true}>
+
+          <ToggleControl
+            label={__('Show controls', 'ekiline-block-collection')}
+            checked={attributes.AddControls}
+            onChange={(AddControls) =>
+              setAttributes({ AddControls })}
+          />
+
+          <ToggleControl
+            label={__('Show indicators', 'ekiline-block-collection')}
+            checked={attributes.AddIndicators}
+            onChange={(AddIndicators) =>
+              setAttributes({ AddIndicators })}
+          />
+
+          <ToggleControl
+            label={__('Auto Play', 'ekiline-block-collection')}
+            checked={attributes.SetAuto}
+            onChange={(SetAuto) => setAttributes({ SetAuto })}
+          />
+
+          <TextControl
+            label={__('Height in pixels, set -1 to see the full height of the screen on the device.', 'ekiline-block-collection')}
+            type='number'
+            value={attributes.SetHeight}
+            onChange={(newval) =>
+              setAttributes({ SetHeight: parseInt(newval) })}
+            min={-1}
+          />
+
+          <TextControl
+            label={__('Transition in milliseconds', 'ekiline-block-collection')}
+            type='number'
+            value={attributes.SetTime}
+            onChange={(newval) =>
+              setAttributes({ SetTime: parseInt(newval) })}
+          />
+
+          <SelectControl
+            label={__('Animation type', 'ekiline-block-collection')}
+            value={attributes.SetAnimation}
+            options={[
+              { label: __('Default', 'ekiline-block-collection'), value: '' },
+              { label: __('Fade', 'ekiline-block-collection'), value: 'fade' },
+              { label: __('Vertical', 'ekiline-block-collection'), value: 'vertical' }
+            ]}
+            onChange={(SetAnimation) =>
+              setAttributes({ SetAnimation })}
+          />
+
+          {/* Opcion de columnas y una varible de diseño, esto ocurrirá con js en el front. */}
+          <RangeControl
+            label={__('Columns', 'ekiline-block-collection')}
+            value={attributes.SetColumns}
+            onChange={(newval) =>
+              setAttributes({ SetColumns: parseInt(newval) })}
+            min={1}
+            max={4}
+          />
+
+        </PanelBody>
+
 
       </InspectorControls>
       {renderVariation()}
