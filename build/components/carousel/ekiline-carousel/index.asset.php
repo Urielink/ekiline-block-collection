adc0bf779bbc8a1a2ca9c2ea4552948e56118b82
Urielink <urielink81@gmail.com>
2025-08-13 19:16:00 -0600

    enlace en carrusel slides ok

--- build/components/carousel/ekiline-carousel/index.asset.php
+++ build/components/carousel/ekiline-carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5f220ce40a2461eb289c');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6e14a970c7ed30cf3c60');
