a59e954bd0ffc5b944bfc287a70217411683d20d
Urielink <urielink81@gmail.com>
2025-08-17 14:10:28 -0600

    siguen ajustes al modo galeria

--- build/components/carousel/ekiline-carousel/index.asset.php
+++ build/components/carousel/ekiline-carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6e14a970c7ed30cf3c60');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7c83ca5292e80e3178a0');
