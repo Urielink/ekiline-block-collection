676ae8fa4ec9aa005b75e57ddd6b95d074d936a5
Urielink <urielink81@gmail.com>
2025-08-27 17:11:41 -0600

    prueba de conteo de slides OK, hay que setearlo.

--- build/components/carousel/ekiline-carousel-slide/index.asset.php
+++ build/components/carousel/ekiline-carousel-slide/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '728fcb5bbe5b0a5eb2fb');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '5e33e08f4a011750e74c');
--- build/components/carousel/ekiline-carousel/index.asset.php
+++ build/components/carousel/ekiline-carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e5b515ea9b4978080446');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9a885215df6bfece05ba');
