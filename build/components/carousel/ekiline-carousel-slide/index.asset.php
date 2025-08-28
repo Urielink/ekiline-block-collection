2443803e70630ea83f8a44f15b97d464aa7ef481
Urielink <urielink81@gmail.com>
2025-08-27 20:16:58 -0600

    estable carrusel manual

--- build/components/carousel/ekiline-carousel-slide/index.asset.php
+++ build/components/carousel/ekiline-carousel-slide/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '5e33e08f4a011750e74c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '770087b38c9b931d51c3');
--- build/components/carousel/ekiline-carousel/index.asset.php
+++ build/components/carousel/ekiline-carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9a885215df6bfece05ba');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3d5bf108f076a9ebdbbd');
