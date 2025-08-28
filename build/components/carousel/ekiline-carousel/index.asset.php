050018e3e1ac14ee6d641b905ec4bbdfe5a2c62b
Urielink <urielink81@gmail.com>
2025-08-27 22:53:06 -0600

    carrusel manual dinámico ok

--- build/components/carousel/ekiline-carousel/index.asset.php
+++ build/components/carousel/ekiline-carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3d5bf108f076a9ebdbbd');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'fe8dd80152571c3d0972');
