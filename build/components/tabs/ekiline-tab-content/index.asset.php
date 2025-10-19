7a838110388fe57e209b584f9044ee9476978643
Urielink <urielink81@gmail.com>
2025-10-18 22:35:11 -0600

    estable, revisar ajustes de ID y herencia. trasladar controles a principal + contextos

--- build/components/tabs/ekiline-tab-content/index.asset.php
+++ build/components/tabs/ekiline-tab-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '94c943e796631e630aad');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f7b58b5a4e6904123c06');
--- src/components/tabs/ekiline-tabs/edit.js
+++ src/components/tabs/ekiline-tabs/edit.js
@@ -3,16 +3,26 @@ import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
 import { replaceSpecialChars } from '../../../shared/collection';
 import { TextControl, Button } from '@wordpress/components';
 
+/**
+ * Imports the icons used in the block.
+ */
+import icons from '../../../shared/icons';
+const { tabsIcon } = icons;
+
+
 export default function Edit({ attributes, setAttributes }) {
 
+  // variables predeterminadas.
   const { numberTabs = 3, template, isConfigured = false } = attributes;
+  // clase de envoltorio.
   const blockProps = useBlockProps({ className: 'tabs-wrapper' });
-
+  // bloques permitidos.
   const allowedBlocks = [
     'ekiline-block-collection/ekiline-tabs-navbar',
     'ekiline-block-collection/ekiline-tabs-container'
   ];
 
+  // Auxiliar, resuelve la plantilla desde attributes.template
   const resolvedTemplate = Array.isArray(template)
     ? template
     : (() => {
@@ -71,33 +81,50 @@ export default function Edit({ attributes, setAttributes }) {
   if (!isConfigured) {
     return (
       <div {...blockProps}>
-        <TextControl
-          label={__('Number of tabs', 'ekiline-block-collection')}
-          type="number"
-          min={2}
-          max={30}
-          value={numberTabs}
-          onChange={(val) => setAttributes({ numberTabs: parseInt(val) || 1 })}
-          help={__('Maximum number of tabs to create 30', 'ekiline-block-collection')}
-        />
-
-        <Button
-          variant="primary"
-          onClick={() => {
-            const newTemplate = createTemplate(numberTabs);
-            setAttributes({
-              template: newTemplate,
-              isConfigured: true
-            });
-          }}
-        >
-          {__('Create tabs', 'ekiline-block-collection')}
-        </Button>
+        <div className='setup-tabs'>
+          <div className="components-placeholder__label">
+            {tabsIcon}
+            <label>{__('Tabs', 'ekiline-block-collection')}</label>
+          </div>
+          <div class="components-placeholder__instructions">
+            {__('Displays content in tab format.', 'ekiline-block-collection')}
+          </div>
+          <div className='tabs-form'>
+            <TextControl
+              label={__('Number of tabs', 'ekiline-block-collection')}
+              type="number"
+              min={2}
+              max={30}
+              value={numberTabs}
+              onChange={(val) => setAttributes({ numberTabs: parseInt(val) || 1 })}
+              className="number-tabs"
+            />
+            <Button
+              variant="primary"
+              onClick={() => {
+                const newTemplate = createTemplate(numberTabs);
+                setAttributes({
+                  template: newTemplate,
+                  isConfigured: true
+                });
+              }}
+              disabled={numberTabs < 2 || numberTabs > 30}
+              className="create-tabs"
+            >
+              {__('Create tabs', 'ekiline-block-collection')}
+            </Button>
+          </div>
+          <label>
+            {
+              numberTabs < 2 || numberTabs > 30
+                ? __('Please enter a number between 2 and 30', 'ekiline-block-collection')
+                : __('You can change the number of tabs later by adding or removing tab link and tab content blocks.', 'ekiline-block-collection')
+            }
+          </label>
+        </div>
       </div>
     );
   }
-
-  console.log(attributes.template)
 
   // Si ya está configurado, mostrar el contenido real
   return (
@@ -105,7 +132,8 @@ export default function Edit({ attributes, setAttributes }) {
       <InnerBlocks
         allowedBlocks={allowedBlocks}
         template={resolvedTemplate}
-        templateLock="all" // Opcional: bloquea estructura base
+        // Opcional: bloquea estructura base
+        // templateLock="all"
       />
     </div>
   );
